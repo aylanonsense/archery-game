68bbe013481435813313387d5e90cdd0197f05b8
Ben Myers <bmyers@appnexus.com>
2014-05-06 23:53:24 -0400

    Reformatted, recolored

--- public/javascripts/Main.js
+++ public/javascripts/Main.js
@@ -1,21 +1,18 @@
 if (typeof define !== 'function') { var define = require('amdefine')(module); }
 define(function() {
 	return function() {
-		var scene, camera, renderer, geometry, material, mesh;
-		init();
+		var scene = new THREE.Scene();
+		var camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 1, 10000);
+		camera.position.z = 1000;
+		var geometry = new THREE.BoxGeometry(200, 200, 200);
+		var material = new THREE.MeshBasicMaterial({ color: 0xffaa00, wireframe: true });
+		var mesh = new THREE.Mesh(geometry, material);
+		scene.add(mesh);
+		var renderer = new THREE.WebGLRenderer();
+		renderer.setClearColor( 0xffffff, 1);
+		renderer.setSize(window.innerWidth, window.innerHeight);
+		document.body.appendChild(renderer.domElement);
 		animate();
-		function init() {
-			scene = new THREE.Scene();
-			camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 1, 10000);
-			camera.position.z = 1000;
-			geometry = new THREE.BoxGeometry(200, 200, 200);
-			material = new THREE.MeshBasicMaterial({ color: 0xff0000, wireframe: true });
-			mesh = new THREE.Mesh(geometry, material);
-			scene.add(mesh);
-			renderer = new THREE.CanvasRenderer();
-			renderer.setSize(window.innerWidth, window.innerHeight);
-			document.body.appendChild(renderer.domElement);
-		}
 		function animate() {
 			requestAnimationFrame(animate);
 			mesh.rotation.x += 0.01;
