9498a7a6a9a246b16ec5ee9d4571a4f8f9a8911f
Ben Myers <bmyers@appnexus.com>
2014-05-07 00:10:30 -0400

    Reoriented cylinder, improved render loop to have ms and time

--- public/javascripts/Main.js
+++ public/javascripts/Main.js
@@ -1,23 +1,35 @@
 if (typeof define !== 'function') { var define = require('amdefine')(module); }
 define(function() {
 	return function() {
+		var START_TIME;
+		var PREV_TIME;
 		var scene = new THREE.Scene();
 		var camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 1, 10000);
 		camera.position.z = 1000;
 		var geometry = new THREE.CylinderGeometry(100, 100, 400, 50, 50, false);
 		var material = new THREE.MeshBasicMaterial({ color: 0xffaa00, wireframe: true });
 		var mesh = new THREE.Mesh(geometry, material);
+		mesh.rotation.x += Math.PI / 2;
 		scene.add(mesh);
 		var renderer = new THREE.WebGLRenderer();
 		renderer.setClearColor( 0xffffff, 1);
 		renderer.setSize(window.innerWidth, window.innerHeight);
 		document.body.appendChild(renderer.domElement);
-		animate();
-		function animate() {
-			requestAnimationFrame(animate);
-			mesh.rotation.x += 0.01;
-			mesh.rotation.y += 0.02;
-			renderer.render(scene, camera);
+		function eachFrame(ms, time) {
+			//TODO
+		}
+		requestAnimationFrame(function(time) {
+			PREV_TIME = Date.now();
+			START_TIME = PREV_TIME - time;
+			loop(time);
+		});
+		function loop(time) {
+			time += START_TIME;
+			var ms = time - PREV_TIME;
+			PREV_TIME = time;
+			eachFrame(ms, time);
+			if(Math.random() > 0.05)
+			window.requestAnimationFrame(loop);
 		}
 	};
 });
